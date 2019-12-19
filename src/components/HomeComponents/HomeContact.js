22f9526cf630770a5b05f7065a67f6120cd4851a
Piotr Talaga <piotrek.talaga@gmail.com>
2019-12-19 15:57:27 +0100

    contact functionality

--- src/components/HomeComponents/HomeContact.js
+++ src/components/HomeComponents/HomeContact.js
@@ -3,23 +3,56 @@ import decoration from '../../assets/Decoration.svg';
 
 
 class HomeContact extends Component {
+
+    state = {
+        name: "",
+        email: "",
+        text:""
+    };
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Nazwa: ${this.state.name}`);
+        console.log(`W: ${this.state.text}`);
+        const submitData = {
+            "data":[
+                this.state.name,
+                this.state.email,
+                this.state.text
+            ]
+        }
+        fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+            method: 'POST',
+            body: JSON.stringify(submitData)
+        }).then( resp => {
+            return resp.text();
+        }).then( text => {
+            console.log(text);
+        });
+    };
     render() {
+        const {name, email, text} = this.state;
         return (
             <section className='contact flex-box'>
                 <div className='contactForm flex-box'>
                     <h1>Skontaktuj się z nami</h1>
                     <img src={decoration}></img>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className='labels flex-box'>
                             <label id="name">Wpisz swoje imię <br/>
-                                <input type="text" name="username"/>
+                                <input type="text" name="name" value={name} onChange={this.handleChange}/>
                             </label>
                             <label id="email">Wpisz swój email <br/>
-                                <input type="email" name="usermail"/>
+                                <input type="email" name="email" value={email} onChange={this.handleChange}/>
                             </label>
                         </div>
                         <label id="textarea">Wpisz swoją wiadomość<br/>
-                            <textarea type="text" name="sometext"
+                            <textarea type="text" name="text" value={text} onChange={this.handleChange}
                                    placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'/>
                         </label>
                         <button>Wyślij</button>
