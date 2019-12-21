3159f9bc4547f44da3a9ed4474da54c601da8111
Piotr Talaga <piotrek.talaga@gmail.com>
2019-12-21 17:37:50 +0100

    logout section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Navigation from "./components/Navigation";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Logout from "./components/Logout";
 
 import {
   HashRouter,
@@ -23,6 +24,7 @@ function App() {
                 <Route exact path='/' component={Home} />
                 <Route path='/login' component={Login} />
                 <Route path='/register' component={Register} />
+                <Route path='/logout' component={Logout} />
             </Switch>
         </>
       </HashRouter>
