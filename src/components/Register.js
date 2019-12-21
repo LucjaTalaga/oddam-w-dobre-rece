f2561775f0ef557162f60797d38128a027f9863f
Piotr Talaga <piotrek.talaga@gmail.com>
2019-12-21 15:31:59 +0100

    register layout

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,35 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import decoration from "../assets/Decoration.svg";
+import {
+    Link,
+} from 'react-router-dom';
 
-class Register extends Component{
+
+class Register extends Component {
     render() {
-        return(
-            <div/>
+        return (
+            <section className='register flex-box'>
+                <div className='registerForm flex-box'>
+                    <h1>Załóż konto</h1>
+                    <img src={decoration}></img>
+                    <form>
+                        <div className='labels flex-box'>
+                            <label id="name">Email<br/>
+                                <input type="text" name="name"/>
+                            </label>
+                            <label id="pass">Hasło<br/>
+                                <input type="password" name="pass"/>
+                            </label>
+                            <label id="passRepeat">Powtórz hasło<br/>
+                                <input type="password" name="passRepeat"/>
+                            </label>
+                        </div>
+                        <button><Link to="/login" className='registerLink'>Zaloguj się</Link></button>
+                        <button>Załóż konto</button>
+                    </form>
+                </div>
+            </section>
         )
     }
 
