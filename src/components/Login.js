1c757c30392946b37ad0c116d6ab365360d2825e
Piotr Talaga <piotrek.talaga@gmail.com>
2019-12-21 13:43:50 +0100

    login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import decoration from "../assets/Decoration.svg";
+import {
+    Link,
+} from 'react-router-dom';
 
-class Login extends Component{
+
+class Login extends Component {
     render() {
-        return(
-            <div/>
+        return (
+            <section className='login flex-box'>
+                <div className='loginForm flex-box'>
+                    <h1>Zaloguj się</h1>
+                    <img src={decoration}></img>
+                    <form>
+                        <div className='labels flex-box'>
+                            <label id="name">Email<br/>
+                                <input type="text" name="name"/>
+                            </label>
+                            <label id="email">Hasło<br/>
+                                <input type="email" name="email"/>
+                            </label>
+                        </div>
+                        <button><Link to="/register" className='loginLink'>Załóż konto</Link></button>
+                        <button>Zaloguj się</button>
+                    </form>
+                </div>
+            </section>
         )
     }
 
