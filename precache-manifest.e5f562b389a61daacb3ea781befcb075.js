5b4a98621b0aa1bf80e28ed239cba5f1604f456c
Piotr Talaga <piotrek.talaga@gmail.com>
2019-12-28 18:39:03 +0100

    Updates

---
+++ precache-manifest.e5f562b389a61daacb3ea781befcb075.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caefe3c4e5c3fa6c0ea412b3107d92f8",
+    "url": "/oddam-w-dobre-rece/index.html"
+  },
+  {
+    "revision": "dd8d12cc0fde840d7de7",
+    "url": "/oddam-w-dobre-rece/static/css/main.7196d9ab.chunk.css"
+  },
+  {
+    "revision": "273827c58e16b5985703",
+    "url": "/oddam-w-dobre-rece/static/js/2.9111fc8c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/oddam-w-dobre-rece/static/js/2.9111fc8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd8d12cc0fde840d7de7",
+    "url": "/oddam-w-dobre-rece/static/js/main.2902c1d3.chunk.js"
+  },
+  {
+    "revision": "da3cefb0e11aabc473b7",
+    "url": "/oddam-w-dobre-rece/static/js/runtime-main.91ba6055.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddam-w-dobre-rece/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddam-w-dobre-rece/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/oddam-w-dobre-rece/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddam-w-dobre-rece/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddam-w-dobre-rece/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddam-w-dobre-rece/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddam-w-dobre-rece/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddam-w-dobre-rece/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddam-w-dobre-rece/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oddam-w-dobre-rece/static/media/logo.5d5d9eef.svg"
+  }
+]);
