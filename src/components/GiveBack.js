f558d2c8393547dbd8a6a5e4d2ce24f6f5d8f173
Piotr Talaga <piotrek.talaga@gmail.com>
2020-01-13 00:07:50 +0100

    adding information section

--- src/components/GiveBack.js
+++ src/components/GiveBack.js
@@ -1,11 +1,20 @@
 import React, {Component} from "react";
 import GiveBackHeader from "./GiveBackComponents/GiveBackHeader";
+import GiveBackInformation from "./GiveBackComponents/GiveBackInformation";
+import GiveBackForm from "./GiveBackComponents/GiveBackForm";
 
 
 class GiveBack extends Component {
+    state = {
+        step: 1
+    };
     render() {
         return (
-            <GiveBackHeader/>
+            <>
+                <GiveBackHeader/>
+                <GiveBackInformation step={this.state.step}/>
+                <GiveBackForm step={this.state.step}/>
+            </>
         )
     }
 }
---
+++ src/components/GiveBackComponents/GiveBackForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class GiveBackForm extends Component {
+    render() {
+        return (
+            <section className='giveBackForm'>
+            </section>
+        )
+    }
+}
+
+export default GiveBackForm;
