90e6f0df4c9135eaceb2e3d7a735ae905841420c
Piotr Talaga <piotrek.talaga@gmail.com>
2020-01-16 18:44:44 +0100

    form step 2 in progress

--- src/components/GiveBackComponents/GiveBackForm.js
+++ src/components/GiveBackComponents/GiveBackForm.js
@@ -1,33 +1,37 @@
 import React, {Component} from "react";
 import FormStepOne from "./FormElements/FormStepOne";
+import FormStepTwo from "./FormElements/FormStepTwo";
 
 class GiveBackForm extends Component {
 
     state = {
-        whatAreYouGiving: null
+        whatAreYouGiving: null,
+        howManyBags: null
     };
+    // metoda, zmieniająca krok postępowania
     formButtonHandler = (e, val) => {
         this.props.stepChanger(val);
     };
+    // metoda, dzięki której użytkownik określa, co oddaje
     handleChange = e => {
       this.setState({
           whatAreYouGiving: e.target.value
       })
     };
+    //metoda, dzięki której użytkownik określa ile worków oddaje
+    howManyBagsHandle = e => {
+        this.setState({
+            howManyBags: e.target.value
+        })
+    };
     render() {
         let {step} = this.props;
         let form;
         if(step == 1){
-            form = <>
-                <FormStepOne handleChange={this.handleChange}/>
-                </>
-
+            form = <FormStepOne handleChange={this.handleChange}/>
         }
         if(step == 2){
-            form =
-                <div>
-                    <p>Krok 2/4 </p>
-                </div>
+            form = <FormStepTwo howManyBagsHandle={this.howManyBagsHandle}/>
         }
         if(step == 3){
             form =
@@ -56,6 +60,7 @@ class GiveBackForm extends Component {
         return (
             <section className='giveBackForm'>
                 {form}
+                <p>oddajesz {this.state.howManyBags} woreczków </p>
                 {(this.props.step<=5 && this.props.step >1) ? <button className='stepButtons' onClick={e => this.formButtonHandler(e, -1)}> Wstecz</button> : null}
                 {this.props.step<=5 ? <button className='stepButtons' onClick={e => this.formButtonHandler(e, 1)}>{this.props.step === 5 ? "Potwierdź" : "Dalej"} </button> : null}
             </section>
